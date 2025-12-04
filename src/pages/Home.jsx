ed9e3066c8ed49488473fc4efd40b853d34d5a68
Timothy Ronco <timothy.ronco@gmail.com>
2025-12-03 19:56:53 -0500

    added skills section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { AboutSection } from "../components/AboutSection";
 import { HeroSection } from "../components/HeroSection";
 import { Navbar } from "../components/Navbar";
+import { SkillsSection } from "../components/SkillsSection";
 import { StarBackground } from "../components/Starbackground";
 import { ThemeToggle } from "../components/ThemeToggle";
 
@@ -18,6 +19,7 @@ export const Home = () => {
                 <main>
                     <HeroSection />
                     <AboutSection />
+                    <SkillsSection />
                     
                 </main>
 
