5f673fcefca576eee7dfb3529df033bfb9031393
Timothy Ronco <timothy.ronco@gmail.com>
2025-12-09 07:41:42 -0500

    added begining projects

---
+++ src/components/ProjectsSection.jsx
@@ -0,0 +1,59 @@
+import { Description } from "@radix-ui/react-toast";
+
+const projects = [
+    {
+        id: 1, 
+        title: "Choose Your Own Adventure Game",
+        description: "a small text based branching storyline where you can choose you own path.",
+        image: "https://picsum.photos/id/8/200.jpg",
+        tags: ["React", "TailwindCSS", "Python"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 2, 
+        title: "Choose Your Own Adventure Game",
+        description: "a small text based branching storyline where you can choose you own path.",
+        image: "https://picsum.photos/id/0/200.jpg",
+        tags: ["React", "TailwindCSS", "Python"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 3, 
+        title: "Choose Your Own Adventure Game",
+        description: "a small text based branching storyline where you can choose you own path.",
+        image: "https://picsum.photos/id/6/200.jpg",
+        tags: ["React", "TailwindCSS", "Python"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+];
+
+
+export const ProjectsSection = () => {
+    return (
+        <section id="projects" className="py-24 px-4 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center"> Featured <span className="text-primary"> Projects </span></h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+                    Here are some recent projects I have created. Each project was hand crafted with attention to detail, performance and user experience.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                    {projects.map((project, key) => 
+                        (<div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                            <div className="h-48 overflow-hidden">
+                                <img src={project.image} alt={project.title}  className="w-full h-full object-cover"/>
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
